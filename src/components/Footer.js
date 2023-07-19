9b8f41961adfb20e158f5a3824cc9206bdc3f55f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-19 13:46:40 +0100

    added files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,68 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      {/* Your footer content here */}
+      <div className="container">
+        <div className="footer-menu">
+          <ul>
+            <li><a href="/">Coursera</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">What We Offer</a></li>
+            <li><a href="/">Leadership</a></li>
+            <li><a href="/">Careers</a></li>
+            <li><a href="/">Catalog</a></li>
+            <li><a href="/">Coursera Plus</a></li>
+            <li><a href="/">Professional Certificates</a></li>
+            <li><a href="/">MasterTrack® Certificates</a></li>
+            <li><a href="/">Degrees</a></li>
+            <li><a href="/">For Enterprise</a></li>
+            <li><a href="/">For Government</a></li>
+            <li><a href="/">For Campus</a></li>
+            <li><a href="/">Become a Partner</a></li>
+            <li><a href="/">Coronavirus Response</a></li>
+          </ul>
+        </div>
+        <div className="footer-menu">
+          <ul>
+            <li><a href="/">Community</a></li>
+            <li><a href="/">Learners</a></li>
+            <li><a href="/">Partners</a></li>
+            <li><a href="/">Beta Testers</a></li>
+            <li><a href="/">Translators</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Tech Blog</a></li>
+            <li><a href="/">Teaching Center</a></li>
+          </ul>
+        </div>
+        <div className="footer-menu">
+          <ul>
+            <li><a href="/">More</a></li>
+            <li><a href="/">Press</a></li>
+            <li><a href="/">Investors</a></li>
+            <li><a href="/">Terms</a></li>
+            <li><a href="/">Privacy</a></li>
+            <li><a href="/">Help</a></li>
+            <li><a href="/">Accessibility</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">Articles</a></li>
+            <li><a href="/">Directory</a></li>
+            <li><a href="/">Affiliates</a></li>
+            <li><a href="/">Modern Slavery Statement</a></li>
+            <li><a href="/">Do Not Sell/Share</a></li>
+            <li><a href="/">Mobile App</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <a href="/">Logo of Certified B Corporation</a>
+          <div className="social-icons">
+            <a href="/"><i className="fab fa-facebook-f"></i></a>
+            <a href="/"><i className="fab fa-linkedin-in"></i></a>
+            <a href="/"><i className="fab fa-twitter"></i></a>
+            <a href="/"><i className="fab fa-youtube"></i></a>
+            <a href="/"><i className="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copy">© 2023 Coursera Inc. All rights reserved.</div>
     </footer>
   );
 };
