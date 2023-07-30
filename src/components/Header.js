9f654990946e775c10a4d54cbbc089d3d2964ac3
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-30 16:59:11 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,57 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <a href="/">Coursera</a>
-      </div>
-      <nav className="nav-links">
-        <Link to="/for-individuals">For Individuals</Link>
-        <Link to="/for-businesses">For Businesses</Link>
-        <a href="/">For Universities</a>
-        <a href="/">For Governments</a>
-        <a href="/">Skip to Main Content</a>
-      </nav>
-      <div className="search-bar">
-        <input type="text" placeholder="What do you want to learn?" />
-        <button>Explore</button>
-      </div>
-      <div className="user-section">
-        <a href="/">Find your New Career</a>
-        <div className="user-profile">
+    <header>
+      <div className="container">
+        <div className="header-left">
           <img
-            src="https://example.com/path-to-your-image"
-            alt="User Profile"
+            src="https://www.coursera.org/images/coursera-logo-plain.svg"
+            alt="Coursera Logo"
           />
-          <span>John Doe</span>
+          <div className="header-links">
+            <Link to="/for-individuals">For Individuals</Link>
+            <Link to="/for-businesses">For Businesses</Link>
+            <a href="#">For Universities</a>
+            <a href="#">For Governments</a>
+            <a href="#">Skip to Main Content</a>
+          </div>
         </div>
-        <div className="user-actions">
-          <a href="/">Welcome back!</a>
-          <a href="/">Tell us about yourself</a>
-          <span>I am currently an MBA graduate</span>
-        </div>
-        <div className="learning-goal">
-          <a href="/">Updating your recommendations</a>
-          <span>Home In Progress Completed</span>
+        <div className="header-right">
+          <button className="explore-button">Explore</button>
+          <div className="dropdown-content">
+            <div className="goals-submenu">
+              <p>Goals</p>
+              <ul>
+                <li>Take a free course</li>
+                <li>Earn a Degree</li>
+                <li>Earn a Certificate</li>
+                <li>Find your new career</li>
+              </ul>
+            </div>
+            <div className="subjects-submenu">
+              <p>Subjects</p>
+              <ul>
+                <li>Data Science</li>
+                <li>Business</li>
+                <li>Computer Science</li>
+                <li>Information Technology</li>
+                <li>Language Learning</li>
+                <li>Health</li>
+                <li>Personal Development</li>
+                <li>Physical Science and Engineering</li>
+                <li>Social Sciences</li>
+                <li>Arts and Humanities</li>
+                <li>Math and Logic</li>
+              </ul>
+            </div>
+          </div>
+          <span className="username">Mohamed Mehdi Gara</span>
+          <div className="user-menu">
+            <a href="#">Welcome back!</a>
+            <a href="#">Tell us about yourself</a>
+            <a href="#">I am currently an MBA graduate</a>
+            <a href="#">Updating your recommendations</a>
+          </div>
         </div>
       </div>
     </header>
