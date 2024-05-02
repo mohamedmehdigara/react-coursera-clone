e7792309fd561915c9d481f2f8796bc7010281da
mohamedmehdigara <garamehdi57@yahoo.com>
2024-05-02 18:40:48 +0100

    added files

--- src/components/ForBusinesses/ForBusinesses.js
+++ src/components/ForBusinesses/ForBusinesses.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./ForBusinesses.css";
 
 const ForBusinesses = () => {
   return (
--- src/components/ForGovernments/ForGovernments.js
+++ src/components/ForGovernments/ForGovernments.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./ForGovernments.css";
 
 const ForGovernments = () => {
   return (
